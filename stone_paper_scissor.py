c05afbc15287fb95bad0fb65db2c76d382aaaf69
Raghav Kohli <raghavkohli011@gmail.com>
2021-05-25 02:11:03 +0530

    Update stone_paper_scissor.py

--- stone_paper_scissor.py
+++ stone_paper_scissor.py
@@ -1,15 +1,15 @@
 import random
-mylist = ["stone","paper","scissor"]
+mylist = ["rock","paper","scissor"]
 randnum = random.choice(mylist)
 def game(user,randnum):
-    if randnum == "stone":
-        if user == "s":
+    if randnum == "rock":
+        if user == "r":
             print("oops! Match tied computer chose: ",randnum)
             return 0
-        elif user == "p":
+        elif user == "s":
             print("oops! you lost computer chose: ",randnum)
             return 0
-        elif user == "c":
+        elif user == "p":
             print("Hurray! you won computer chose: ",randnum)
             return 1
     elif randnum == "paper":
@@ -19,21 +19,21 @@ def game(user,randnum):
         elif user == "p":
             print("oops! Match tied computer chose: ",randnum)
             return 0
-        elif user == "c":
+        elif user == "r":
             print("oops! you lost computer chose: ",randnum)
             return 0
     elif randnum == "scissor":
-        if user == "s":
+        if user == "p":
             print("oops! you lost computer chose: ",randnum)
             return 0
-        elif user == "p":
+        elif user == "r":
             print("Hurray! you won computer chose: ",randnum)
             return 1
-        elif user == "c":
+        elif user == "s":
             print("oops! Match tied computer chose: ",randnum)
             return 0     
 finalscore = 0
-user = input("Choose Between Stone(s),Paper(p),Scissor(c)\n")
+user = input("Choose Between Rock(r),Paper(p),Scissor(s)\n")
 score = game(user,randnum)
 finalscore = finalscore + score
 i=0
@@ -41,7 +41,7 @@ while i<1:
     play=input("Play Again! (y/n)\n")
     if(play=="y"):
         randnum = random.choice(mylist)
-        user = input("Choose Between Stone(s),Paper(p),Scissor()\n")
+        user = input("Choose Between Rock(r),Paper(p),Scissor(s)\n")
         score = game(user,randnum)
         finalscore = finalscore + score
     else:
